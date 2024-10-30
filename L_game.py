3fa38b650de688097bcd6b22a80f0269ad271a08
dsawoo <wooa345@gmail.com>
2024-10-30 13:10:00 -0700

    Update L_game.py
    Created basic outline (Might need some other functions)

--- L_game.py
+++ L_game.py
@@ -24,3 +24,67 @@ last n moves; to switch to computer-vs-computer play; to rotate or flip the
 board (to aid visualization for the human); to save the game; etc. The plot of 
 the board could be as simple as ASCII text symbols, or as fancy as you like.
 '''
+#Grid size and diretions
+GRID_SIZE = 4
+NORTH, SOUTH, EAST, WEST = 0, 1, 2, 3
+
+
+#L piece orientation
+
+
+
+
+class GameState:
+
+
+    def init(self, player_1, player_2, neutral_piece):
+        #initalize tje game state with  the startiong postions for the L pieces and neutral 
+        #player 1 and player 2 represent the corner position and orientation of each L-PIece
+        #neutral piece is the 2 1x1 ciricle piece
+    
+
+
+    def is_valid():
+        #check if a given (x,y) position is within the grid boundries
+        #return true if within bound, if not return false
+    
+    def get_Piece_Position():
+        #caclucate the (x,y) positions based on corner and oritentation
+        #return a list of all (x,y) postions that the L piece occupies
+    
+    def is_position_free():
+        #check if positon is free within the bound, and not occupied by other player
+        #no overlapping with neurtal piece
+    
+
+    def apply_move():
+        #update the L piece postion
+    
+
+
+    def get_legal_move():
+        #get all possible legal moves for the given player's L-Piece
+        #get current L-Piece position and oreientation
+    
+
+    def eval():
+        #get legal moves for both players
+        #calculate the difference in the number of moves between the max and min players
+        #return the difference as the heuristic score
+    
+
+    def miniMax(gamestate, depth, alpha, beta, max__min_player):
+        #recurisve minimax function with alpha-beta pruning
+        #gamestate: the current state of the game
+        #depth: max search depth for minimax algo
+        #alpha, beta: values for alpha-beta pruning
+        #max__min_player: boolean telling wheterh we are max or min
+        #return best score
+
+
+    def keyboard_input():
+        #choose inputting format on keyboard for user input
+    
+
+    def main():
+        #for displaying main menu like how we did in wordle or similar projects
