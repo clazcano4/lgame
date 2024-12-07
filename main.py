29936b793a631f3ddc76dfc362bbf2c6dd1a7fe2
dsawoo <wooa345@gmail.com>
2024-12-06 17:25:12 -0800

    move everything to main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
-from util import Stack
-from keyboard import userInput
+
 
 class GameState:
     def __init__(self, player_positions, dot_positions, current_player):
@@ -381,6 +380,35 @@ def update_display():
     screen.blit(input_surface, (15, 430))  # Render the input text
 
     pygame.display.flip()
+
+
+def userInput(move_input):
+    #Parses the move input and returns the parsed data.
+    move = move_input.strip().split()
+
+    if len(move) < 3 or len(move) > 7:
+        raise ValueError("Invalid input format. Enter a valid move (e.g., '1 2 E' or '1 2 E 4 3 1 1').")
+
+    try:
+        x = int(move[0])
+        y = int(move[1])
+        orientation = move[2]
+
+        if len(move) == 3:
+            return x, y, orientation, None
+
+        elif len(move) == 7:
+            oldNeutralX = int(move[3])
+            oldNeutralY = int(move[4])
+            newNeutralX = int(move[5])
+            newNeutralY = int(move[6])
+            neutral_move = ((oldNeutralX, oldNeutralY), (newNeutralX, newNeutralY))
+            return x, y, orientation, neutral_move
+
+        else:
+            raise ValueError("Invalid input format.")
+    except ValueError:
+        raise ValueError("Invalid input. Make sure to enter integers for coordinates and a valid direction.")
 
 
 
